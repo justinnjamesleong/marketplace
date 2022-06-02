49dddbabdf35bd00f9fdbe0025f88ff84d5b9052
poh mingfu <718pohmf@gmail.com>
2022-06-02 15:08:21 +0800

    updated user profile path

--- app/javascript/controllers/paymentmodal_controller.js
+++ app/javascript/controllers/paymentmodal_controller.js
@@ -3,13 +3,22 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["popUp", "cards", "credit"];
 
-  open() {
+  opentopup() {
+    // default is item-cards-list
+    this.cardsTarget.classList.add("d-none");
     this.popUpTarget.classList.toggle("modal");
-    this.cardsTarget.classList.toggle("d-none");
+    this.popUpTarget.classList.remove("d-none");
     // this.popUpTarget.classList.remove("fade");
     console.log(this.popUpTarget.classList);
     console.log(this.cardsTarget.classList);
   }
+
+  opencreation() {
+    this.cardsTarget.classList.toggle("d-none");
+    this.popUpTarget.classList.add("d-none");
+  }
+
+  openauction() {}
 
   close() {
     // this.cardsTarget.classList.remove("d-none");
