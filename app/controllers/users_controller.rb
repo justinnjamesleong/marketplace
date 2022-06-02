65f4307d2ea1c70c6e120e106b537a701ee7a160
poh mingfu <718pohmf@gmail.com>
2022-06-02 17:39:46 +0800

    updated profile tags

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @items = Item.where("creator_id = ?", @user.id)
+    @auctions = Auction.all
   end
 
   def edit
--- app/javascript/controllers/paymentmodal_controller.js
+++ app/javascript/controllers/paymentmodal_controller.js
@@ -1,24 +1,30 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["popUp", "cards", "credit"];
+  static targets = ["popUp", "auctioncards", "credit", "listcards"];
 
   opentopup() {
     // default is item-cards-list
-    this.cardsTarget.classList.add("d-none");
-    this.popUpTarget.classList.toggle("modal");
+    this.auctioncardsTarget.classList.add("d-none");
+    this.listcardsTarget.classList.add("d-none");
     this.popUpTarget.classList.remove("d-none");
+    this.popUpTarget.classList.toggle("modal");
     // this.popUpTarget.classList.remove("fade");
     console.log(this.popUpTarget.classList);
     console.log(this.cardsTarget.classList);
   }
 
   opencreation() {
-    this.cardsTarget.classList.toggle("d-none");
+    this.listcardsTarget.classList.toggle("d-none");
+    this.auctioncardsTarget.classList.add("d-none");
     this.popUpTarget.classList.add("d-none");
   }
 
-  openauction() {}
+  openauction() {
+    this.auctioncardsTarget.classList.toggle("d-none");
+    this.listcardsTarget.classList.add("d-none");
+    this.popUpTarget.classList.add("d-none");
+  }
 
   close() {
     // this.cardsTarget.classList.remove("d-none");
