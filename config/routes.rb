a69b06de1da8c2a37d5fedbf5c2dfea9271b4dec
poh mingfu <718pohmf@gmail.com>
2022-06-02 12:19:25 +0800

    double checking ensuring all files are uploaded

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   resources :franchises
   resources :auctions
   devise_for :users
-  resources :users
+  resources :users do
+    post 'users/:id', to: 'users#topup'
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
