b8dc7b4c711926fc8a748b6439cccc2605369ba4
Minquan Tan <minch_tan@hotmail.com>
2022-06-04 12:04:18 +0800

    working on items seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,49 +1,93 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
-
-10.times do
-  franchise = Franchise.new(name: Faker::Appliance.brand)
-  franchise.save
-
-  itemtype = ItemType.new(name: Faker::ElectricalComponents.active)
-  itemtype.save
+# seeding franchises
+puts "seeding franchises now"
+franchises = %w[superman batman spiderman gundam pokemon disney harry\ potter star\ wars middle\ earth james\ bond]
+franchises.each do |franchise|
+  puts "creating franchise instance for #{franchise}"
+  capitalized_franchise = franchise.capitalize
+  new_franchise = Franchise.new(name: capitalized_franchise)
+  new_franchise.save!
 end
+puts "finished franchise seeding"
+puts "#{Franchise.all.count} franchises seeded"
 
-10.times do
-  user = User.new(username:Faker::Name.name,email:Faker::Internet.email,password:"124566")
-  user.save
+# seeding itemtypes
+puts "seeding itemtypes now"
+itemtypes = %w[painting drawing accessory sculpture figure prop constume clothing houseware other]
+itemtypes.each do |itemtype|
+  puts "creating franchise instance for #{itemtype}"
+  capitalized_itemtype = itemtype.capitalize
+  new_itemtype = ItemType.new(name: capitalized_itemtype)
+  new_itemtype.save!
 end
+puts "finished itemtype seeding"
+puts "#{ItemType.all.count} itemtypes seeded"
 
-10.times do
-  flyknit = Item.new(name: Faker::Science.element, description: Faker::Space.galaxy)
-  flyknit.franchise = Franchise.all.sample
-  flyknit.item_type = ItemType.all.sample
-  flyknit.owner = User.all.sample
-  flyknit.creator = User.all.sample
-
-  flyknit.save!
-
-  review = Review.new(comment: Faker::Space.nasa_space_craft, rating: (1..5).to_a.sample)
-  review.item = Item.all.sample
-  review.user = User.all.sample
-  review.save
-
-  time = Time.now()
-  auction = Auction.new(start_time:time, end_time:time+(10000..20000).to_a.sample, minimum_bid:(100..500).to_a.sample)
-  auction.item = Item.all.sample
-  auction.save
-
-  bid = Bid.new(bid_time:time)
-  bid.buyer = User.all.sample
-  bid.bid_amount = (100..1000).to_a.sample
-  bid.auction = Auction.all.sample
-  bid.save
-
-  puts "done!"
+# seeding users
+puts "seeding users now"
+usernames = %w[john mary sue anish luqman xiaoming takada jean anand karpov]
+default_password = "password"
+usernames.each do |username|
+  puts "creating franchise instance for #{username}"
+  email = "#{username}@gmail.com"
+  new_user = User.new
+  new_user.username = username
+  new_user.password = default_password
+  new_user.email = email
+  new_user.save!
 end
+puts "finished itemtype seeding"
+puts "#{ItemType.all.count} itemtypes seeded"
+
+puts "-------------------------------------------------------------------------"
+puts "phew! getting a little tired"
+puts "-------------------------------------------------------------------------"
+puts "but i'll keep going you lucky dog!"
+puts "-------------------------------------------------------------------------"
+
+puts "seeding items now"
+# name, description, franchise, itemtype, user->owner, user->creator
+creators = User.all.first(5)
+puts "only 5 users will have items. Each of those 5 users will have 2 items"
+puts "those users are:"
+creators.each do |creator|
+  p creator.username
+end
+# %w[superman batman spiderman gundam pokemon disney harry\ potter star\ wars middle\ earth james\ bond]
+item1 = Item.new
+item1.name = "A man & his car"
+item1.description = "25th anniversary celebration"
+item1.franchise = Franchise.all.last # james bond
+item1.item_type = Franchise.all.first # painting
+item1.creator = creators[0] # john
+item1.owner = creators[0]
+
+puts "item 1 created"
+item1.photo = img("bond")
+item1.photo.attach(io: File.open("bond.jpg"))
+puts "image attached to item 1"
+
+#   flyknit.franchise = Franchise.all.sample
+#   flyknit.item_type = ItemType.all.sample
+#   flyknit.owner = User.all.sample
+#   flyknit.creator = User.all.sample
+
+#   flyknit.save!
+
+#   review = Review.new(comment: Faker::Space.nasa_space_craft, rating: (1..5).to_a.sample)
+#   review.item = Item.all.sample
+#   review.user = User.all.sample
+#   review.save
+
+#   time = Time.now()
+#   auction = Auction.new(start_time:time, end_time:time+(10000..20000).to_a.sample, minimum_bid:(100..500).to_a.sample)
+#   auction.item = Item.all.sample
+#   auction.save
+
+#   bid = Bid.new(bid_time:time)
+#   bid.buyer = User.all.sample
+#   bid.bid_amount = (100..1000).to_a.sample
+#   bid.auction = Auction.all.sample
+#   bid.save
+
+#   puts "done!"
+# end
